0b48473dfd365e340e59f6b11c8652d8166e798e
btchoum <bertrand.tchoumkeu@gmail.com>
2022-09-18 11:21:54 -0500

    create resource group

---
+++ tf/main.tf
@@ -0,0 +1,31 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.2.5"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# create a resource group to hold all our resources
+resource "azurerm_resource_group" "todosrg" {
+  name     = "todos-app-rg"
+  location = "centralus"
+}
+
+# create an app service plan in the resource group created above
+resource "azurerm_service_plan" "todossp" {
+  name                = "todos-service-plan"
+  resource_group_name = azurerm_resource_group.todosrg.name
+  location            = azurerm_resource_group.todosrg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
